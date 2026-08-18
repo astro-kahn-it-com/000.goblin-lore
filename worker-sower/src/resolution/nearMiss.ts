eb274e915e8d6569da50908d5f30b46946da46cb
Brad Henderson <elliotbradly@gmail.com>
2026-08-17 23:11:12 -0400

    feat: update

--- worker-sower/src/resolution/nearMiss.ts
+++
@@ -1,6 +0,0 @@
-export function applyNearMiss(val: number) {
-    return {
-        false_relief: true,
-        next_climb_start_offset: 1
-    };
-}
--- worker-sower/src/resolution/relationalCatharsis.ts
+++
@@ -1,5 +0,0 @@
-export function applyCatharsis(val: {resentment: number, scar_tissue: number}) {
-    return {
-        scar_tissue: val.scar_tissue + 1
-    };
-}
